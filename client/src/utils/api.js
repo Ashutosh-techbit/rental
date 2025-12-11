c6f4cbcb897cce66bfb7b3cc434d1c27f93c6296
Ashutosh-techbit <bashutosh168@gmail.com>
2025-12-11 08:59:02 +0530

    updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,8 +2,10 @@ import axios from "axios";
 import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
+const apiBaseURL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
+
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: apiBaseURL,
 });
 
 export const getAllProperties = async () => {
